b3a7c6b13e193bee7a0673edafb09aea2c835aed
Ivana Stojadinović <ivanastojadinovic001@gmail.com>
2024-02-03 16:48:43 +0100

    first commit

---
+++ sandbox.js
@@ -0,0 +1,13 @@
+function displayTime(){
+    let date = new Date();
+    let h = date.getHours(); // 0 - 23
+    let m = date.getMinutes(); // 0 - 59
+    let s = date.getSeconds(); // 0 - 59
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
+}
+    setInterval(displayTime, 1000);
